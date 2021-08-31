3c74bcd2b000ccd373d436693b46db3ac13fa492
lean <lean.dsoria@gmail.com>
2021-08-31 15:28:44 -0300

    commit

--- app.js
+++ app.js
@@ -34,3 +34,31 @@ const sectionObserver = new IntersectionObserver(revealSection, {
 sections.forEach(function (section) {
   sectionObserver.observe(section);
 });
+
+// Lazy loading images
+// const imgTargets = document.querySelectorAll('img[data-src]');
+
+// const loadImage = function (entries, observer) {
+//   const [entry] = entries;
+//   if (!entry.isIntersecting) return;
+//   // Replacing src with data-src
+//   entries.forEach(e => {
+//     e.target.src = e.target.dataset.src;
+//   });
+
+//   entry.target.addEventListener('load', function () {
+//     entry.target.classList.remove('lazy-img');
+//     console.log(entries);
+//   });
+// };
+
+// const imgObserver = new IntersectionObserver(loadImage, {
+//   root: null,
+//   threshold: 0.1,
+//   rootMargin: '100px',
+// });
+
+// imgTargets.forEach(img => {
+//   imgObserver.observe(img);
+//   console.log(img.src);
+// });
